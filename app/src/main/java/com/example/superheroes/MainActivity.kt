2f8961628cfc4dbc53af354b6fc607d9cdf5d07a
kishantalekar <kishantalekar024@gmail.com>
2024-07-16 00:51:58 +0530

    done

---
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.superheroes
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.data.Hero
+import com.example.superheroes.data.HeroSource
+import com.example.superheroes.ui.theme.SuperheroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperheroesTheme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+               HeroApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroApp() {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar()
+    }) {
+
+        HeroList(heroes = HeroSource.heroes,contentPadding=it)
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun HeroAppPreview() {
+    HeroApp()
+}
